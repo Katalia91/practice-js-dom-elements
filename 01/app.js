81757a9f5081bedf635f3c3e82293857786fc460
Natalia Kur <natalia.kur@yahoo.com>
2025-11-19 16:16:21 +0100

    Complete task #01

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,10 @@
-console.log('DOM');
+console.log("DOM");
+
+const element = document.querySelector(
+  ".comments__item.comments__item--newest"
+);
+
+if (element) {
+  const dataSetElement = element.querySelectorAll("[data-info]");
+  console.log(dataSetElement.length);
+}
