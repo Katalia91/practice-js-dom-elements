a738f37d5ff6fbf255cd9338e17dd8ef91662ac2
Natalia Kur <natalia.kur@yahoo.com>
2025-11-21 17:02:46 +0100

    Complete task #02

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,9 @@
-console.log('DOM');
+console.log("DOM");
+
+const dataUrl = document.querySelectorAll("[data-url");
+dataUrl.forEach(function (el) {
+  const url = el.dataset.url;
+  if (!el.hasAttribute("href")) {
+    el.setAttribute("href", url);
+  }
+});
