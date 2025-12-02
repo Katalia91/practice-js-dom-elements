02cbb567738877298306a8b9f17114f4f264a81d
Natalia Kur <natalia.kur@yahoo.com>
2025-12-02 13:30:47 +0100

    Complete task #05

--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,29 @@
-console.log('DOM');
+console.log("DOM");
 
-const curr = document.querySelector('.js-curr');
+const curr = document.querySelector(".js-curr");
+const removeBtn = document.createElement("button");
+const currParent = curr.parentElement;
+
+currParent.appendChild(removeBtn);
+const removeBtnText = document.createTextNode("Usuń z koszyka");
+removeBtn.appendChild(removeBtnText);
+
+const siblings = currParent.children;
+const siblingsArray = [...siblings];
+
+siblingsArray.forEach(function (el) {
+  if (!el.classList.contains("js-curr")) {
+    el.classList.add("sibling");
+  }
+});
+if (currParent.nextElementSibling.classList.contains("article")) {
+  currParent.nextElementSibling.setAttribute("title", "nextElementSibling");
+}
+
+const articleParrent = currParent.parentElement;
+const p = document.createElement("p");
+const lastBtn = articleParrent.lastElementChild.lastElementChild;
+articleParrent.lastElementChild.insertBefore(p, lastBtn);
+
+const clonedArticle = currParent.cloneNode(true);
+articleParrent.insertBefore(clonedArticle, currParent);
